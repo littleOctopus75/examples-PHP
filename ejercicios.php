4813910715f6ff0ac6a67e87da491123d4587311
Kralos <solrac@aulavirtual.umar.mx>
2023-10-09 17:01:20 -0600

    Create ejercicios.php

---
+++ ejercicios.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 1. Muestra los números del 1 al 10
+ */
+
+/**
+ * 2. Poner un hola mundo dentro de un html.
+ */
+
+/**
+ * 3. POner una imagen en un img con la ruta de la imagen.
+ */
+
+/**
+ * 4. Crear un select en HTML y poner los números del 1 al 10 como opciones.
+ */
+
+/**
+ * 5. Muestra los números del 1 al 10
+ */
+
+?>
