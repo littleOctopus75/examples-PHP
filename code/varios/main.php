bff98dd3771251bb7f35abe9c79766e250191fa6
littleOctopus75 <127283227+littleOctopus75@users.noreply.github.com>
2023-10-20 16:16:23 -0600

    Update main.php
    Implementando seguridad en la sesion con tiempo de inactividad

--- code/varios/main.php
+++ code/varios/main.php
@@ -4,6 +4,18 @@
 	if(!isset($_SESSION["usuario"])){
 		header("Location: error.php");
 	}
+	$inactividad = 10;
+    // Comprobar si $_SESSION["timeout"] está establecida
+    if(isset($_SESSION["timeout"])){
+        // Calcular el tiempo de vida de la sesión (TTL = Time To Live)
+        $sessionTTL = time() - $_SESSION["timeout"];
+        if($sessionTTL > $inactividad){
+            session_destroy();
+            header("Location: logout.php");
+        }
+    }
+    // El siguiente key se crea cuando se inicia sesión
+    $_SESSION["timeout"] = time();
 ?>
 
 <h1> Aceptado ... :D </h1>
