1c5031b880e2820bf550165233238c76d0d1fafd
Jonathan Eduardo <jonnymartinez.pzl1@gmail.com>
2023-11-11 21:13:18 -0600

    apuntes

--- code/varios/databases/conex_mysqli.php
+++ code/varios/databases/conex_mysqli.php
@@ -5,12 +5,23 @@ $_db = "company"; // 👍
 //$_db = "compania"; // 👎
 $_user = "root";
 $_pass = "";
+$_host = "127.0.0.1";
+$_puerto = 3306;
 
-$_conection = new mysqli($_host,$_user,$_pass,$_db);
+$_conection = new mysqli($_host,$_user,$_pass,$_db, $_puerto);
 
 if($_conection->connect_errno){
     echo "Error: ". $_conection->connect_error;
 }
 
 echo "Connected successfully to MySQL database :D !!! 😊";
+
+echo $_conection->host_info . " uwu 😊 \n";
+
+
+if (!$_conection->query("DROP TABLE IF EXISTS test") || !$_conection->query("CREATE TABLE test(id INT)")){
+    echo "Fallo";
+}
+
+
 mysqli_close($_conection);
--- code/varios/databases/conex_pdo.php
+++ code/varios/databases/conex_pdo.php
@@ -8,6 +8,13 @@ $_pass = "";
 
 try {
     $_conection = new PDO('mysql:host='.$_host.'; dbname='.$_db, $_user, $_pass);
+
+    //hellouda
+    $stmt = $_conection->prepare("INSERT INTO test (id) VALUES (?)");
+    $id = 1;
+    $stmt->bindParam(1, $id); 
+    $stmt->execute();
+
     echo "Connected successfully to MySQL database :D !!! 😊";
 } catch (PDOException $e) {
     echo $e->getMessage();
